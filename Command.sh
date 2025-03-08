5b34ce6ce64f3dd80347a9ead8922e74b5cd1324
Bhakta Bam <183859583+3BThakuri@users.noreply.github.com>
2025-03-08 23:04:16 +0530

    Create Command.sh

---
+++ Command.sh
@@ -0,0 +1,6 @@
+#display commands output
+echo "Present working directory: `pwd `"
+echo "Long listing of files `ls -l `"
+echo -n "Hostname `hostname` "
+echo "Date and time `date`"
+printf "Hello how are you"
